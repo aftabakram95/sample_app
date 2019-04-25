f0f952fcef6b146493821a631e2daf63777ca21a
Aftab Akram <aftabakram04@gmail.com>
2019-04-25 11:46:49 +0500

    Finish Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,44 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User successfully created"
+      redirect_to @user
+    else
+      flash.now[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "User was successfully updated"
+        redirect_to @user
+      else
+        flash.now[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
